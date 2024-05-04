a32d8e9a6f1230cfc790f1837d05ed5711148715
parkjuno0309 <98671569+parkjuno0309@users.noreply.github.com>
2024-05-04 18:43:23 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // Convert images < 8kb to base64 strings
+              name: 'images/[name].[hash:8].[ext]', // Output path for images
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
